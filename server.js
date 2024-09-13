9d5037624783dca90996c3f3482c874ec9034cf4
Nico Reincheld <nicoreincheld@gmail.com>
2024-09-13 11:45:19 -0400

    Setup route for keywords

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-import express from "express";
-import "dotenv/config";
+import express from 'express';
+import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 8081;
-import industryRoute from "./routes/industryRoutes.js";
-import cors from "cors";
+import industryRoute from './routes/industryRoutes.js';
+import keyRoute from './routes/keyRoutes.js';
+import cors from 'cors';
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use("/product", industryRoute);
+app.use('/product', industryRoute);
+
+app.use('/keywords', keyRoute);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
