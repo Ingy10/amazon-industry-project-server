93e917c5c0b7dafcb2a0d8f37d446b647d162af3
Howard Huang <howard.huang3+git@gmail.com>
2024-09-12 21:31:45 -0400

    Create GET comments request

--- routes/industryRoutes.js
+++ routes/industryRoutes.js
@@ -1,5 +1,21 @@
 import express from "express";
+import initKnex from "knex";
+import configuration from "../knexfile.js";
 
 const router = express.Router();
+const knex = initKnex(configuration);
+
+// for list of warehouses
+router.get("/", async (_req, res) => {
+  try {
+    const data = await knex("reviews");
+    if (data.length === 0) {
+        return res.status(404).json({ message: "No reviews found" });
+      }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send(`Error retrieving Reviews: ${error}`);
+  }
+});
 
 export default router;
