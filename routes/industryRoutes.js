90d9e9a52fe33a05d99f277f6f9d3fb4be8f6b20
Howard Huang <howard.huang3+git@gmail.com>
2024-09-13 11:48:55 -0400

    Add keyword gets

--- routes/industryRoutes.js
+++ routes/industryRoutes.js
@@ -18,4 +18,30 @@ router.get("/", async (_req, res) => {
   }
 });
 
+// for list of keywords
+router.get("/keywords", async (_req, res) => {
+    try {
+      const data = await knex("keywords");
+      if (data.length === 0) {
+          return res.status(404).json({ message: "No keywords found" });
+        }
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).send(`Error retrieving Keywords: ${error}`);
+    }
+  });
+
+// for specific keyword
+router.get("/keywords/:key", async (_req, res) => {
+    try {
+      const data = await knex("keywords").where({ keyword: key });
+      if (data.length === 0) {
+          return res.status(404).json({ message: "No keywords found" });
+        }
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).send(`Error retrieving Keywords: ${error}`);
+    }
+  });
+
 export default router;
