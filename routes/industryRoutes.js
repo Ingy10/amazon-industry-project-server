971836033e3c65c235115ffd239dcccf313e84c5
Kyle Ingham <kyleingy@proton.me>
2024-09-13 04:18:39 -0600

    Created initial structure of POST request.

--- routes/industryRoutes.js
+++ routes/industryRoutes.js
@@ -2,4 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/:productId/review/add", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.description) {
+      res.status(400).json("Title and review description are required");
+    }
+
+    const updatedReviews = await knex("reviews").insert(req.body);
+    const newReviewId = updatedReviews[0];
+    const newReview = await knex("reviews")
+      .where({ id: newReviewId })
+      .select("*");
+    res.status(200).json(newReview[0]);
+  } catch (error) {
+    res.status(500).json(`${error}`);
+  }
+});
+
 export default router;
